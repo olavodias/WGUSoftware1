41f91208333f5cc091d7e99b527610c460c6fe12
Olavo Henrique Dias <olavodias@hotmail.com>
2017-10-17 20:19:59 -0600

    Added the Inventory System Main Screen.

--- InventorySystem/src/inventorysystem/InventorySystem.java
+++ InventorySystem/src/inventorysystem/InventorySystem.java
@@ -6,10 +6,17 @@
 package inventorysystem;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,8 +28,7 @@ public class InventorySystem extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        
 //        btn.setOnAction(new EventHandler<ActionEvent>() {
 //            
 //            @Override
@@ -31,15 +37,92 @@ public class InventorySystem extends Application {
 //            }
 //        });
         
-        btn.setOnAction(event->
-          System.out.println("Hello World"));
+        /***********************************************************************
+         * Create the Main Screen
+         **********************************************************************/
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        /******************************************
+         * Header
+         *****************************************/
         
-        Scene scene = new Scene(root, 300, 250);
+        /* Label for Title */
+        Label lblHeader_Title = new Label();
+        lblHeader_Title.setText("Inventory Management System");
+        lblHeader_Title.getStyleClass().add("darkblue-window-title-text");
         
-        primaryStage.setTitle("Hello World!");
+        /* Horizontal Box */
+        HBox hBoxHeader = new HBox();
+        hBoxHeader.getChildren().add(lblHeader_Title);
+        hBoxHeader.setMinHeight(50);
+        hBoxHeader.setAlignment(Pos.CENTER_LEFT);
+        hBoxHeader.setPadding(new Insets(20));
+        hBoxHeader.getStyleClass().add("darkblue-window-title");
+
+        /***********************************************************************
+         * Bottom
+         **********************************************************************/
+        
+        /* Button to Exit */
+        Button btnBottom_Exit = new Button();
+        btnBottom_Exit.setText("EXIT");
+        btnBottom_Exit.getStyleClass().add("darkblue-button");
+        btnBottom_Exit.setPrefSize(100, 25);
+        
+        btnBottom_Exit.setOnAction(e -> Platform.exit());
+        
+        /* Box to wrap the Button */
+        HBox hBoxBottom = new HBox(btnBottom_Exit);
+        hBoxBottom.setMinHeight(50);
+        hBoxBottom.setAlignment(Pos.CENTER_RIGHT);
+        hBoxBottom.setPadding(new Insets(20));
+        
+        /***********************************************************************
+         * Center
+         **********************************************************************/
+
+        /***************************************
+         * Left Panel
+         **************************************/
+        Label lblTeste = new Label();
+        lblTeste.setText("Testing");
+        
+        StackPane paneParts = GUIHelper.createPanel(lblTeste);
+
+        /***************************************
+         * Right Panel
+         **************************************/
+        StackPane paneProducts = GUIHelper.createPanel(lblTeste);
+
+        /***************************************
+         * GridPane for Both Panels
+         **************************************/
+        GridPane gridCenter = new GridPane();
+        
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(50);
+        
+        gridCenter.getColumnConstraints().addAll(col1, col1);
+        
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(100);
+        
+        gridCenter.getRowConstraints().add(row1);
+        
+        gridCenter.add(paneParts, 0, 0);
+        gridCenter.add(paneProducts, 1, 0);
+        
+        /***********************************************************************
+         * Border Pane
+         **********************************************************************/
+        BorderPane border = new BorderPane();
+        border.setTop(hBoxHeader);
+        border.setBottom(hBoxBottom);
+        border.setCenter(gridCenter);
+        
+        Scene scene = new Scene(border, 800, 400);
+        scene.getStylesheets().add("inventorysystem/InventorySystem.css");
+        
+        primaryStage.setTitle("Inventory Management System");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
