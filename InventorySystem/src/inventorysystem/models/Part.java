893bee8c2d78b8c3608ed632a14ed5d0e0e3cb34
olavo.dias <olavo.dias@IBC-OF-WL0179.na.autoliv.int>
2017-10-27 14:07:59 -0600

    Added properties and javadoc for Part class.

--- InventorySystem/src/inventorysystem/models/Part.java
+++ InventorySystem/src/inventorysystem/models/Part.java
@@ -29,25 +29,150 @@ public abstract class Part {
     /******************************************
      * Property Getters/Setters
      ******************************************/
+    
+    /**
+     * Retrieves the Part ID
+     * @return      An integer containing the Part ID
+     */
     public int getID()
     {
         return id.get();
     }
     
-    public void setID(int _id)
+    /**
+     * Sets the Part ID
+     * @param _id   An integer containing the Part ID
+     */
+    public final void setID(int _id)
     {
         id.set(_id);
     }
 
+    /**
+     * Retrieves the Part Name
+     * @return      A string containing the Part Name
+     */
     public String getName()
     {
         return name.get();
     }
     
-    public void setName(String _name)
+    /**
+     * Sets the Part Name
+     * @param _name     A string containing the Part Name
+     */
+    public final void setName(String _name)
     {
         name.set(_name);
     }
     
+    /**
+     * Retrieves the Part Price
+     * @return      A double containing the Part Price
+     */
+    public double getPrice()
+    {
+        return price.get();
+    }
     
+    /**
+     * Sets the Part Price
+     * @param _price    A double containing the Part Price 
+     */
+    public final void setPrice(double _price)
+    {
+        price.set(_price);
+    }
+
+    /**
+     * Retrieves the Part Quantity In Stock 
+     * @return      An integer containing the quantity in stock
+     */
+    public int getInStock()
+    {
+        return inStock.get();
+    }
+    
+    /**
+     * Sets the Part Quantity In Stock
+     * @param _inStock  An integer containing the quantity in stock
+     */
+    public final void setInStock(int _inStock)
+    {
+        inStock.set(_inStock);
+    }
+
+    /**
+     * Returns the Part Maximum Stock Quantity
+     * @return  An integer containing the Maximum Stock Quantity
+     */
+    public int getMax()
+    {
+        return max.get();
+    }
+    
+    /**
+     * Sets the Part Maximum Stock Quantity
+     * @param _max  An integer containing the Maximum Stock Quantity
+     */
+    public final void setMax(int _max)
+    {
+        max.set(_max);
+    }
+
+    /**
+     * Returns the Part Minimum Stock Quantity
+     * @return  An integer containing the Minimum Stock Quantity
+     */
+    public int getMin()
+    {
+        return min.get();
+    }
+    
+    /**
+     * Sets the Part Minimum Stock Quantity
+     * @param _min  An integer containing the Minimum Stock Quantity
+     */
+    public final void setMin(int _min)
+    {
+        min.set(_min);
+    }
+
+    /******************************************
+     * Constructors
+     ******************************************/
+    public Part()
+    {
+        this(0, "");
+    }
+    
+    public Part(int _id, String _name)
+    {
+        this(_id, _name, 0);
+    }
+
+    public Part(int _id, String _name, double _price)
+    {
+        this(_id, _name, _price, 0);
+    }
+
+    public Part(int _id, String _name, double _price, int _inStock)
+    {
+        this(_id, _name, _price, _inStock, 0);
+    }
+    
+    public Part(int _id, String _name, double _price, int _inStock, int _min)
+    {
+        this(_id, _name, _price, _inStock, _min, 0);
+    }
+    
+    public Part(int _id, String _name, double _price, int _inStock, int _min, int _max)
+    {
+        this.setID(_id);
+        this.setName(_name);
+        this.setPrice(_price);
+        this.setInStock(_inStock);
+        this.setMin(_min);
+        this.setMax(_max);
+    }
 }
